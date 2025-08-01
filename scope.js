14464e8f9446283b8502eae20f61eb786c1908f3
Sreehari K I <sreeharikunuthala@gmail.com>
2025-08-01 23:07:56 +0530

    Add files via upload

---
+++ scope.js
@@ -0,0 +1,13 @@
+let global="123";
+function test(){
+  if(true){
+    let a=20;
+    const b=30;
+    var c=40;
+  }
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+console.log(global);;
+console.log(c);
